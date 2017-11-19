c21949310f1bae8d8fe3584f396aecb191bbe931
amichaelgrant <a.michael.grant@gmail.com>
2017-11-19 13:00:41 -0700

    Update Configuration.js

--- Configuration.js
+++ Configuration.js
@@ -6,5 +6,5 @@ exports.REDIS_PORT= 6379;
 
 exports.SMTP_CONTACT = "Support";
 exports.SMTP_USER = "werkwerk5000bc@gmail.com";
-exports.SMTP_PASS = ".W3rk3r6c";
+exports.SMTP_PASS = "";
 exports.SMTP_FROM = "werkwerk5000bc@gmail.com";
